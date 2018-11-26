3c9c044f38f71cd9f1b6ab4089ba8e3a8c8d0ca5
Vilda Tri Lestari Simbolon <15.8921@stis.ac.id>
2018-11-26 19:29:04 +0700

    Form udah bener

--- application/views/input_data_pdb_produksi_harga_konstan.php
+++ application/views/input_data_pdb_produksi_harga_konstan.php
@@ -6,54 +6,81 @@
 		        	  <div class="row">
 					    <form class="col s12">
 					      	<div class="row">
-					        	<div class="input-field col s12">
-					          		<input placeholder="2015" id="first_name" type="text" class="validate">
-					          		<label for="first_name">Tahun</label>
+					        	<div class="input-field col s6">
+					          		<input placeholder="2015" id="tahun" name="tahun" type="text" class="validate">
+					          		<label for="tahun">Tahun</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">Pertanian, Kehutanan dan Perikanan</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colA" name="colA" type="text" class="validate">
+					          		<label for="colA">Pertanian, Kehutanan dan Perikanan</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colB" name="colB" type="text" class="validate">
+					          		<label for="colB">Pertambangan dan Penggalian</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colC" name="colC" type="text" class="validate">
+					          		<label for="colC">Industri Pengolahan</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colD" name="colD" type="text" class="validate">
+					          		<label for="colD">Pengadaan listrik dan Gas</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colE" name="colE" type="text" class="validate">
+					          		<label for="colE">Pengadaan Air, Pengelolaan Sampah, Limbah dan Daur Ulang</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colF" name="colF" type="text" class="validate">
+					          		<label for="colF">Konstruksi</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colG" name="colG" type="text" class="validate">
+					          		<label for="colG">Perdagangan Besar dan Eceran; Reparasi Mobil dan Sepeda Motor</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colH" name="colH" type="text" class="validate">
+					          		<label for="colH">Transportasi dan Pergudangan</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colI" name="colI" type="text" class="validate">
+					          		<label for="colI">Penyediaan Akomodasi dan Makan Minum</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colJ" name="colJ" type="text" class="validate">
+					          		<label for="colJ">Informasi dan Komunikasi</label>
 					        	</div>
 					        	<div class="input-field col s6">
-					          		<input placeholder="123456789" id="first_name" type="text" class="validate">
-					          		<label for="first_name">First Name</label>
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colK" name="colK" type="text" class="validate">
+					          		<label for="colK">Jasa Keuangan dan Asuransi</label>
 					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colL" name="colL" type="text" class="validate">
+					          		<label for="colL">Real Estate</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colM" name="colM" type="text" class="validate">
+					          		<label for="colM">Jasa Perusahaan</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colO" name="colO" type="text" class="validate">
+					          		<label for="colO">Administrasi Pemerintah, Pertanahan, dan Jaminan Sosial Wajib</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colP" name="colP" type="text" class="validate">
+					          		<label for="colP">Jasa Pendidikan</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colQ" name="colQ" type="text" class="validate">
+					          		<label for="colQ">Jasa Kesehatan dan Kegiatan Sosial</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					          		<input placeholder="dalam Rp milyar, contoh: 72592.1" id="colR" name="colR" type="text" class="validate">
+					          		<label for="colR">Jasa Lainnya</label>
+					        	</div>
+					        	<div class="input-field col s6">
+					        		<a class="waves-effect waves-light btn"><i class="material-icons left">cloud</i>submit</a>
+					        	</div> 
 					      	</div>
 					    </form>
 					  </div>
