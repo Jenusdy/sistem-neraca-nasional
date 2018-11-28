874261317bb0ed3baf50734d5d6d1efb133036b5
Oka Widhyartha <15.8658@stis.ac.id>
2018-11-28 14:05:06 +0700

    perbaikan col row home

--- application/views/home.php
+++ application/views/home.php
@@ -1,23 +1,26 @@
-<br><br><br>
-<div class="container">
-	<div class="col s12 m7">
-    	<div class="card horizontal">
-      		<div class="card-stacked">
-        		<div class="card-content">
-        			<h4 class="header">Fitur</h4>
-        			<div class="row">
-        				<a href="<?php echo base_url('home/pdb_produksi_harga_konstan');?>" class="col l12 waves-effect waves-light btn-large"><i class="material-icons left">insert_chart</i>PDB Produksi harga Konstan</a> <br>
-        			</div>
-        			<div class="row">
-        				<a href="<?php echo base_url('home/pdb_produksi_harga_berlaku');?>" class="col l12 waves-effect waves-light btn-large"><i class="material-icons left">account_balance</i>PDB Produksi harga Berlaku</a> <br>
-        			</div>
-        			<div class="row"> 
-        				<a href="<?php echo base_url('home/pdb_pengeluaran');?>" class="col l12 waves-effect waves-light btn-large"><i class="material-icons left">attach_money</i>PDB Pengeluaran</a> <br>
-        			</div>
-        			
-        			
-        		</div>
-        	</div>
+<div class="container" style="margin-top:20px;">
+	<div class="row">
+		<div class="col s12">
+			<div class="card white">
+        <div class="card-content">
+          <span class="card-title">Fitur</span>
+					<hr>
+					<div class="row">
+						<div class="col s12">
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_konstan');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">insert_chart</i>PDB Produksi harga Konstan</a>
+						</div>
+						<div class="col s12" style="margin-top:10px;">
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_berlaku');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">account_balance</i>PDB Produksi harga Berlaku</a>
+						</div>
+						<div class="col s12" style="margin-top:10px;">
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_pengeluaran');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">attach_money</i>PDB Pengeluaran</a> <br>
+						</div>
+					</div>
         </div>
-    </div>
+        <div class="card-action">
+					Made in localhost by 4KS1
+        </div>
+      </div>
+		</div>
+	</div>
 </div>
