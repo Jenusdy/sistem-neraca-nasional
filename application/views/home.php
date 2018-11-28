0f21d27ae1328f5e2614bca914c0be5506944a1f
Oka Widhyartha <15.8658@stis.ac.id>
2018-11-28 14:30:54 +0700

    perbaikan warna dan icon home

--- application/views/home.php
+++ application/views/home.php
@@ -3,22 +3,22 @@
 		<div class="col s12">
 			<div class="card white">
         <div class="card-content">
-          <span class="card-title">Fitur</span>
+          <span class="card-title light-green-text text-darken-3"><i class="far fa-list-alt"></i> Fitur</span>
 					<hr>
 					<div class="row">
 						<div class="col s12">
-							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_konstan');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">insert_chart</i>PDB Produksi harga Konstan</a>
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_konstan');?>" class="light-green lighten-1 waves-effect waves-light btn-large"><i class="material-icons left">insert_chart</i>PDB Produksi harga Konstan</a>
 						</div>
 						<div class="col s12" style="margin-top:10px;">
-							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_berlaku');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">account_balance</i>PDB Produksi harga Berlaku</a>
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_produksi_harga_berlaku');?>" class="light-green lighten-1 waves-effect waves-light btn-large"><i class="material-icons left">account_balance</i>PDB Produksi harga Berlaku</a>
 						</div>
 						<div class="col s12" style="margin-top:10px;">
-							<a style="width:100%;" href="<?php echo base_url('home/pdb_pengeluaran');?>" class="waves-effect waves-light btn-large"><i class="material-icons left">attach_money</i>PDB Pengeluaran</a> <br>
+							<a style="width:100%;" href="<?php echo base_url('home/pdb_pengeluaran');?>" class="light-green lighten-1 waves-effect waves-light btn-large"><i class="material-icons left">attach_money</i>PDB Pengeluaran</a> <br>
 						</div>
 					</div>
         </div>
-        <div class="card-action">
-					Made in localhost by 4KS1
+        <div class="grey lighten-4 card-action right-align">
+					Made by 4KS1 with <span class='red-text'>&#10084;</span>
         </div>
       </div>
 		</div>
--- application/views/template/navbar.php
+++ application/views/template/navbar.php
@@ -1,5 +1,5 @@
-<nav class="light-blue lighten-1" role="navigation">
-  <div class="nav-wrapper container"><a id="logo-container" href="<?php echo base_url();?>" class="brand-logo">National Accounts</a>
+<nav class="lime darken-1" role="navigation">
+  <div class="nav-wrapper container"><a id="logo-container" href="<?php echo base_url();?>" class="brand-logo"><img src='<?= base_url('assets/img/favicon.png'); ?>'/> National Accounts</a>
     <ul class="right hide-on-med-and-down">
       <!-- <li><a href="<?php echo base_url('home/upload_data');?>">Input Data</a></li> -->
     </ul>
